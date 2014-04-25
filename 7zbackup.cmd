c199a09315da21c4f261fa7287fee2b0f304bbce
Fadhil Mandaga <firodj@gmail.com>
2014-04-25 11:41:54 +0700

    Rebase all

--- 7zbackup.cmd
+++ 7zbackup.cmd
@@ -2,11 +2,15 @@
 set projname=wxAlcatel
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-3 delims=/: " %%a in ('time /t') do (set mytime=%%a-%%b-%%c)
-set szapp="c:\Program Files\7-Zip\7z.exe"
-if not exist %szapp% set szapp=.\7za.exe
-if exist %szapp% (
-%szapp% a %projname%_%mydate%_%mytime%.7z *.* -x@7zexclude.txt -r -ssw -scsUTF-8 -mx=9
+if defined ProgramFiles (
+set szapp="%ProgramFiles%\7-Zip\7z.exe"
 ) else (
-echo Could not find 7z executable.
+set szapp=".\7za.exe"
 )
+if exist %szapp% (
+%szapp% a %projname%_%mydate%_%mytime%.7z *.* -x@7zexclude.inf -r -ssw -scsUTF-8 -mx=9
+) else (
+echo ERROR: 7-zip not found.
+)
+set szapp=
 pause
--- wxAlcatel/vidterm_svn_ci.bat
+++
@@ -1,2 +0,0 @@
-@echo password: Hu7WM3Ap7kK9
-svn ci --username firodj@gmail.com -m %1 %2 %3 %4 %5 %6 %7 %8 %9
